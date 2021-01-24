c7a414f6aee6372d30e1ef80798aaff627436eb3
IVANKSHU VAGHANI <69029612+ivan570@users.noreply.github.com>
2021-01-24 15:56:10 +0530

    Create Strange Birthday Party.cpp

---
+++ cpp/codeforces/Strange Birthday Party.cpp
@@ -0,0 +1,48 @@
+// https://codeforces.com/problemset/problem/1470/A
+#include <iostream>
+#include <vector>
+#include <algorithm>
+ 
+void party()
+{
+    int n, m;
+    std::cin >> n >> m;
+    std::vector<int> k(n + 1), c(m + 1);
+    for (int i = 1; i <= n; ++i)
+        std::cin >> k[i];
+ 
+    for (int i = 1; i <= m; ++i)
+        std::cin >> c[i];
+ 
+    int last = 1;
+    long long int ans = 0;
+    std::sort(k.rbegin(), k.rend());
+    // for (int value : k)
+    //     std::cout << value << " ";
+    std::cout << "\n";
+    for (int i = 0; i < n; ++i)
+    {
+        if (c[last] >= c[k[i]])
+        {
+            ans += c[k[i]];
+            // std::cout << "i" << ans << " ";
+        }
+        else
+        {
+            ans += c[last];
+            last++;
+            // std::cout << "e" << ans << " ";
+        }
+    }
+ 
+    std::cout << ans << "\n";
+}
+ 
+int main()
+{
+    int count;
+    std::cin >> count;
+    for (int i = 0; i < count; ++i)
+        party();
+    return 0;
+}
